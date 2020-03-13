0853f2c0e51fffdc706a605ed4cf0894c5c26e07
Tuur Lievens <tuur.lievens@hotmail.com>
2020-03-13 23:44:29 +0100

    Tweak settings
    - More mobile friendly
    - Less animations and distractions
    - Dark mode
    - Use minified scripts
    - Add zoom, remove exif plugins

--- theme/public/setup.js
+++ theme/public/setup.js
@@ -7,21 +7,36 @@ $(document).ready(function() {
         counter: true,
         videojs: true,
         exThumbImage: 'data-exthumbimage',
+        // Custom options
+        mode: 'lg-slide',
+        speed: 200,
+        hideBarsDelay: 4500,
+        closable: false,
+        hideControlOnEnd: true,
+        preload: 2,
+        swipeThreshold: 30,
+        // Custom plugin options
+        thumbContHeight: 168,
+        thumbWidth: 100,
+        showThumbByDefault: false,
+        pause: 3000,
+        zoom: true,
+        scale: 0.5,
     });
 
     // Init justified gallery for albums
     $("#albums").justifiedGallery({
         // selector: '.albumList'
         margins: 10,
-        rowHeight: 300,
-        maxRowHeight: 325,
+        rowHeight: 225,
+        maxRowHeight: "150%",
     });
 
     // Init justified gallery for photos
     $("#media").justifiedGallery({
         selector: '.albumList',
-        margins: 10,
-        rowHeight: 250,
-        maxRowHeight: 275,
+        margins: 5,
+        rowHeight: 165,
+        maxRowHeight: "150%",
     });
 });
