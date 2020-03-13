2116042ea59fd309a4dbf0455106750ad58c8858
Tuur Lievens <tuur.lievens@hotmail.com>
2020-03-14 00:46:59 +0100

    Add album filter

--- theme/public/setup.js
+++ theme/public/setup.js
@@ -40,18 +40,41 @@ $(document).ready(function() {
         maxRowHeight: "150%",
     });
 
-    // Remember scroll position
-    if (window.location.hash) {
-        // Scroll to previous place
-        const distance = parseInt(window.location.hash.slice(1));
-        setTimeout(() => {
-            if (distance) {
-                window.scrollTo({top: distance, behavior: "auto"});
-            }
-        }, 250);
+    // Filter albums by caption name
+    if (document.querySelector('#albums').children.length) {
+        document.querySelector('#filter').addEventListener('change', function(event) {
+            const query = event.target.value;
+            document.querySelectorAll('.justified-gallery > a').forEach(function(el) {
+                const caption = el.querySelector('h3').innerText;
+                if (caption.includes(query) || query.length < 2) {
+                    el.classList.remove('filtered');
+                } else {
+                    el.classList.add('filtered');
+                }
+            });
+            $('#albums').justifiedGallery({ filter: ':not(.filtered)' });
+        });
+    } else {
+        document.querySelector('#filter').style.display = 'none';
     }
-    // Put scroll position in history so page can scroll down when going back
-    setInterval(() => {
-        history.replaceState({}, window.title, "#"+(window.pageYOffset || window.scrollY));
+
+    // Recall saved scroll position
+    if (window.location.hash) {
+        const parts = window.location.hash.slice(1).split('|');
+
+        if (parts[1]) { // filter
+            document.querySelector('#filter').value = parts[1];
+            document.querySelector('#filter').dispatchEvent(new Event('change'));
+        }
+        if (parts[0]) { // offset
+            setTimeout(function(){ window.scrollTo({ top: parseInt(parts[0]) }) }, 250);
+        }
+    }
+
+    // Update history state with current variables
+    setInterval(function() {
+        const offset = window.pageYOffset || window.scrollY;
+        const filter = document.querySelector('#filter').value;
+        history.replaceState({offset: offset, filter: filter}, window.title, '#' + offset + '|' + filter);
     }, 1500);
 });
