3a44b9f76770c667a635d0d64b4abccf0fd15a57
Tuur Lievens <tuur.lievens@hotmail.com>
2020-03-14 15:35:15 +0100

    Refactor state manegement and add random sort

--- theme/public/setup.js
+++ theme/public/setup.js
@@ -1,4 +1,20 @@
+// Helper function to create a reproducable random integer
+// Source: http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+
+let seed = 0; // not random when seed = 0
+function getSeededRandom() {
+    var max = max || 1;
+    var min = min || 0;
+
+    seed = (seed * 9301 + 49297) % 233280;
+    var rnd = seed / 233280;
+
+    return min + rnd * (max - min);
+}
+
 $(document).ready(function() {
+    let initialized = false;
+
     $("#media").lightGallery({
         controls: true,
         loop : false,
@@ -41,47 +57,94 @@ $(document).ready(function() {
         maxRowHeight: "150%",
     });
 
+    // Update history state with current variables
+    function pushState() {
+        if (!initialized) return;
+        const offset = window.pageYOffset || window.scrollY;
+        const filter = document.querySelector('#filter').value;
+        if (offset || filter || seed) {
+            history.replaceState(
+                { offset: offset, filter: filter, seed: seed },
+                window.title,
+                '#' + offset + '|' + filter + '|' + seed
+            );
+        } else if (window.location.hash !== '#') {
+            history.replaceState({}, window.title, '#');
+        }
+    }
+    setInterval(pushState, 1500);
+
     // Filter albums by caption name
-    if (document.querySelector('#albums').children.length) {
-        document.querySelector('#filter').addEventListener('change', function(event) {
-            const query = event.target.value;
-            document.querySelectorAll('.justified-gallery > a').forEach(function(el) {
-                const caption = el.querySelector('h3').innerText;
-                if (caption.includes(query) || query.length < 2) {
-                    el.classList.remove('filtered');
-                } else {
-                    el.classList.add('filtered');
-                }
-            });
-            $('#albums').justifiedGallery({ filter: ':not(.filtered)' });
+    function filter(query) {
+        document.querySelectorAll('.justified-gallery > a').forEach(function(el) {
+            const caption = el.querySelector('h3').innerText;
+            if (caption.includes(query) || query.length < 2) {
+                el.classList.remove('filtered');
+            } else {
+                el.classList.add('filtered');
+            }
         });
-    } else {
-        document.querySelector('#filter').style.display = 'none';
+        $('#albums').justifiedGallery({ filter: ':not(.filtered)' });
+        window.scrollTo({ top: 0 });
+        pushState();
     }
 
-    // Recall saved scroll position
+    // Randomize album sorting
+    function randomize(inputSeed) {
+        if (seed) {
+            $('#albums').justifiedGallery({sort: function(a, b){
+                return a.querySelector('h3').innerText < b.querySelector('h3').innerText ? -1 : 1;
+            }});
+            seed = 0;
+            document.querySelector('#randomize').innerHTML = "Randomize";
+        } else {
+            const originalSeed = parseFloat(inputSeed) || Math.random(); // create new seed if not supplied
+            seed = originalSeed;
+    
+            const gallery = document.querySelector('.justified-gallery');
+            const elements = Array.from(gallery.children).sort(function () { return getSeededRandom(seed) - 0.5; });
+    
+            gallery.innerHTML = '';
+            elements.forEach(el => gallery.appendChild(el));
+            seed = originalSeed;
+            $('#albums').justifiedGallery();
+            document.querySelector('#randomize').innerHTML = "Sort alphabetically";
+        }
+
+        window.scrollTo({ top: 0 });
+        pushState();
+    }
+
+    // Add event listeners or hide controls
+    if (document.querySelector('#albums').children.length) {
+        document.querySelector('#filter').addEventListener('change', function(event) { filter(event.target.value); });
+        document.querySelector('#randomize').addEventListener('click', randomize);
+    } else {
+        document.querySelector('#controls').style.display = 'none';
+    }
+
+    // Recall saved variables
+    let scrollOffset = 0;
     if (window.location.hash) {
         const parts = window.location.hash.slice(1).split('|');
 
+        if (parseInt(parts[0])) { // offset
+            scrollOffset = parseInt(parts[0]);
+        }
         if (parts[1]) { // filter
             document.querySelector('#filter').value = parts[1];
-            document.querySelector('#filter').dispatchEvent(new Event('change'));
+            filter(parts[1]);
         }
-        if (parts[0]) { // offset
-            setTimeout(function(){ window.scrollTo({ top: parseInt(parts[0]) }) }, 250);
+        if (parseFloat(parts[2])) { // seed
+            randomize(parts[2]);
         }
     }
-
-    // Update history state with current variables
-    setInterval(function() {
-        const offset = window.pageYOffset || window.scrollY;
-        const filter = document.querySelector('#filter').value;
-        if (offset || filter || window.location.hash) {
-            history.replaceState(
-                {offset: offset, filter: filter},
-                window.title,
-                '#' + offset + '|' + filter
-            );
+    $("#albums").justifiedGallery().on('jg.complete', function () {
+        if (!initialized) {
+            setTimeout(function(){
+                if (scrollOffset) window.scrollTo({ top: scrollOffset });
+                initialized = true;
+            }, 50);
         }
-    }, 1500);
+    });
 });
